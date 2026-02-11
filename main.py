8fe48dfa705b466c04fc16abdfcaa1b219a60ffd
kastertrooy2 <kaster.trooy2@gmail.com>
2026-02-11 17:38:44 +0500

    Fix executor handling and response logging

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 test
+--- [EXECUTOR] Acting as: kastertrooy ---
+--- [EXECUTOR] Forking kastertrooy2/max-test-bounty-2... ---
+--- [EXE EXCEPTION] 'object' ---
+--- [EXECUTOR] PR failed or already exists ---
+--- [DB] Task 3926213988 marked as PR_FAILED ---
+[SHEETS] Response: true
